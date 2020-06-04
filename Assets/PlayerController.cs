ecc80c9f4e6d1baee0335be2657938b0267fa9a6
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-06-04 22:11:44 +0530

    Added FSM for Jump state

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,31 +6,49 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+
+    private enum State { idle, running, jumping }
+    private State state=State.idle;
 
     private void Start() {
         rb=GetComponent<Rigidbody2D>();
         anim=GetComponent<Animator>();
+        coll= GetComponent<Collider2D>();
     }
 
     private void Update(){
+
         float HDirection=Input.GetAxis("Horizontal");
+
         if(HDirection<0){
-            // rb.velocity=new Vector2(-5,0);
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale=new Vector2(-1,1);
-            anim.SetBool("running", true);
+           
         }
         else if(HDirection>0){
-            // rb.velocity=new Vector2(-5,0);
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale=new Vector2(1,1);
-            anim.SetBool("running", true);
+            
         }else{
-            anim.SetBool("running", false);
+
         }
-        if(Input.GetKey(KeyCode.W)){
-            // rb.velocity=new Vector2(-5,0);
-            rb.velocity = new Vector2(rb.velocity.x, 5);
+        if(Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground)){
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            state=State.jumping;
+        }
+        velocityState();
+        anim.SetInteger("state",(int)state);
+    }
+
+    private void velocityState(){
+        if(state==State.jumping){
+
+        }else if(Mathf.Abs(rb.velocity.x) > 2f){
+            state=State.running;
+        }else{
+            state=State.idle;
         }
     }
 }
